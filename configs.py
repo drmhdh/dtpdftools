226ddd90c51ab1c72c089c67b4fe7b4fbb84ab32
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-25 22:39:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -51,13 +51,13 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
     # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
+    #CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    #TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
-    USER_NAME = os.environ.get("USER_NAME", "")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH", "")
+    #USER_NAME = os.environ.get("USER_NAME", "")
+    #API_ID = int(os.environ.get("API_ID"))
+    #API_HASH = os.environ.get("API_HASH", "")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     #AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
@@ -72,12 +72,12 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    #DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     
     # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
+   # OUO_IO_API_KEY = ""
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
