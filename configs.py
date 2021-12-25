47b845f1cdb768dfd3c9bae29de103058d2e272f
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-25 23:34:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -69,6 +69,7 @@ class Config(object):
     #MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
