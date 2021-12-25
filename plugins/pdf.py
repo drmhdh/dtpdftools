ce57dadefb86f7e644d25143d0f245e9efc12c12
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-25 15:53:33 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -1201,9 +1201,9 @@ async def extract(bot, message):
             
             
 @Client.on_callback_query()
-async def answer(client: Client, callbackQuery: CallbackQuery): 
+async def answer(Client, CallbackQuery): 
     
-    edit = callbackQuery.data
+    edit = CallbackQuery.data
     
     #if edit == "strtDevEdt":
     if edit.startswith("strtDevEdt"):
