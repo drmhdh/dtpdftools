f958da9d4e866f8902cc81c5b54af2bb65766b12
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-27 13:11:19 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -42,7 +42,7 @@ async def generate_custom_thumbnail(bot, update):
        
         reply_message = update.reply_to_message
         if reply_message.media_group_id is not None:
-            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
+            download_location = Config.DOWNLOAD_LOCATIONS + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
             save_final_image = download_location + str(round(time.time())) + ".jpg"
             list_im = os.listdir(download_location)
             if len(list_im) == 2:
@@ -97,7 +97,7 @@ async def savethumbnail(bot, update):
     update.from_user.id, update.text, "savethumbnail"
     if update.reply_to_message.media_group_id is not None:
         # album is sent
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+        download_location = Config.DOWNLOAD_LOCATIONS + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
         # create download directory, if not exist
         if not os.path.isdir(download_location):
             os.makedirs(download_location)
@@ -107,7 +107,7 @@ async def savethumbnail(bot, update):
         )
     else:
         # received single photo
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        download_location = Config.DOWNLOAD_LOCATIONS + "/" + str(update.from_user.id) + ".jpg"
         await bot.download_media(
             message=update.reply_to_message,
             file_name=download_location
@@ -130,7 +130,7 @@ async def delete_thumbnail(bot, update):
         return
     #TRChatBase(update.from_user.id, update.text, "deletethumbnail")
     update.from_user.id, update.text, "deletethumbnail"
-    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+    download_location = Config.DOWNLOAD_LOCATIONS + "/" + str(update.from_user.id)
     try:
         os.remove(download_location + ".jpg")
         # os.remove(download_location + ".json")
